1cafe9817332211b6d8d7e702c7eb83913ec9f37
doshchinese <doshchinese@gmail.com>
2021-11-02 15:01:23 +0300

    first comit

---
+++ src/components/Education/Education.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import classes from './Education.module.css';
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
+import { MdSchool } from 'react-icons/md';
+import { MdWork } from 'react-icons/md';
+import { FaSchool } from 'react-icons/fa';
+
+class Education extends Component {
+    render() {
+        return (
+                <div className={classes.box} id="education">
+                    <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2' animateOnce="true" initiallyVisible ={true}>
+                     <span  className={classes.head}>MY JOURNEY</span>
+                    <section className={classes.container}>
+                        <div className={classes.container_content}>
+                            <div className={classes.row}>
+                                <div className={classes.row_md_12}>
+                                    <div className={classes.timeline_centered}>
+                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                            <article className={classes.timeline_entry}>
+                                                <div className={`${classes.timeline_icon} ${classes.timeline_icon_2}`} >
+                                                    <FaSchool />     
+                                                </div>
+                                                <div className={classes.label}>
+                                                <h2 >  KAKAMEGA HIGH SCHOOL  <span>2010-2013</span></h2>
+                                                    <p>
+                                                        Completed my K.C.S.E with an Outstanding academic achievement of Grade A- (76 points).
+                                                    </p>
+                                                </div>
+                                            </article>
+                                        </ScrollAnimation>
+                                       <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                            <article className={classes.timeline_entry}>
+                                                <div className={classes.timeline_icon} >
+                                                <MdSchool />
+                                                </div>
+                                                <div className={classes.label}>
+                                                <h2 > MOI UNIVERSITY <span>2014-2018</span></h2>
+                                                    <p> Graduated with a Bsc Computer Science 2nd Class Honors from <a href='https://www.mu.ac.ke/' style={{color:'Highlight'}}>Moi University</a>. </p>
+                                                </div>
+                                            </article>
+                                        </ScrollAnimation>
+                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                            <article>
+                                                <div className={`${classes.timeline_icon} ${classes.timeline_icon_4}`} >
+                                                    <MdWork />     
+                                                </div>
+                                                <div className={classes.label}>
+                                                <h2 >Internship <span>2018-2019</span></h2>
+                                                <p>Completed 1 year Internship at <a href='https://crystaltech.co.ke/' style={{color:'Highlight'}}> Crystal Technologies Limited</a> </p>
+                                                </div>
+                                                <div className={classes.timeline_entry_inner}><div  className={classes.timeline_icon_3||classes.color_none}></div></div>
+                                            </article>
+                                        </ScrollAnimation>
+                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                            <article>
+                                                <div className={classes.label}>
+                                                <h2 >Web App Developer <span>2019-2021</span></h2>
+                                                <p> Worked as a web app developer for <a href='https://ilaniconcepts.com/' style={{color:'Highlight'}}> Ilani Concepts </a></p>
+                                                </div>
+                                                <div className={classes.timeline_entry_inner}><div  className={classes.timeline_icon_3||classes.color_none}></div></div>
+                                            </article>
+                                        </ScrollAnimation>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    </ScrollAnimation>
+                </div>
+     
+        )
+    }
+}
+
+export default Education;
